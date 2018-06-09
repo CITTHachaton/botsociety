51e2f1a6d546b8ba7118c63711215460237dd3b6
Byron Sepúlveda <bsepulveda@localhost.localdomain>
2018-06-08 20:13:52 -0400

    datos a insertar

---
+++ inserta_datos.sql
@@ -0,0 +1,224 @@
+INSERT INTO estado_civil VALUES(1, 'Soltero/a');
+INSERT INTO estado_civil VALUES(2, 'Casado/a');
+INSERT INTO estado_civil VALUES(3, 'Viudo/a');
+INSERT INTO estado_civil VALUES(4, 'Divorciado/a');
+
+INSERT INTO sexo VALUES (1, 'Masculino');
+INSERT INTO sexo VALUES (2, 'Femenino');
+
+INSERT INTO genero VALUES (1, 'Hombre');
+INSERT INTO genero VALUES (2, 'Mujer');
+INSERT INTO genero VALUES (3, 'Otro');
+
+INSERT INTO nacionalidad VALUES(	1	,TRIM('	afgano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	2	,TRIM('	alemán	 ')) ; 
+INSERT INTO nacionalidad VALUES(	3	,TRIM('	árabe	 ')) ; 
+INSERT INTO nacionalidad VALUES(	4	,TRIM('	argentino	 ')) ; 
+INSERT INTO nacionalidad VALUES(	5	,TRIM('	australiano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	6	,TRIM('	belga	 ')) ; 
+INSERT INTO nacionalidad VALUES(	7	,TRIM('	boliviano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	8	,TRIM('	brasileño	 ')) ; 
+INSERT INTO nacionalidad VALUES(	9	,TRIM('	camboyano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	10	,TRIM('	canadiense	 ')) ; 
+INSERT INTO nacionalidad VALUES(	11	,TRIM('	chileno	 ')) ; 
+INSERT INTO nacionalidad VALUES(	12	,TRIM('	chino	 ')) ; 
+INSERT INTO nacionalidad VALUES(	13	,TRIM('	colombiano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	14	,TRIM('	coreano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	15	,TRIM('	costarricense	 ')) ; 
+INSERT INTO nacionalidad VALUES(	16	,TRIM('	cubano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	17	,TRIM('	danés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	18	,TRIM('	ecuatoriano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	19	,TRIM('	egipcio	 ')) ; 
+INSERT INTO nacionalidad VALUES(	20	,TRIM('	salvadoreño	 ')) ; 
+INSERT INTO nacionalidad VALUES(	21	,TRIM('	escocés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	22	,TRIM('	español	 ')) ; 
+INSERT INTO nacionalidad VALUES(	23	,TRIM('	estadounidense	 ')) ; 
+INSERT INTO nacionalidad VALUES(	24	,TRIM('	estonio	 ')) ; 
+INSERT INTO nacionalidad VALUES(	25	,TRIM('	etiope	 ')) ; 
+INSERT INTO nacionalidad VALUES(	26	,TRIM('	filipino	 ')) ; 
+INSERT INTO nacionalidad VALUES(	27	,TRIM('	finlandés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	28	,TRIM('	francés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	29	,TRIM('	galés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	30	,TRIM('	griego	 ')) ; 
+INSERT INTO nacionalidad VALUES(	31	,TRIM('	guatemalteco	 ')) ; 
+INSERT INTO nacionalidad VALUES(	32	,TRIM('	haitiano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	33	,TRIM('	holandés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	34	,TRIM('	hondureño	 ')) ; 
+INSERT INTO nacionalidad VALUES(	35	,TRIM('	indonés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	36	,TRIM('	inglés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	37	,TRIM('	iraquí	 ')) ; 
+INSERT INTO nacionalidad VALUES(	38	,TRIM('	iraní	 ')) ; 
+INSERT INTO nacionalidad VALUES(	39	,TRIM('	irlandés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	40	,TRIM('	israelí	 ')) ; 
+INSERT INTO nacionalidad VALUES(	41	,TRIM('	italiano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	42	,TRIM('	japonés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	43	,TRIM('	jordano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	44	,TRIM('	laosiano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	45	,TRIM('	letón	 ')) ; 
+INSERT INTO nacionalidad VALUES(	46	,TRIM('	letonés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	47	,TRIM('	malayo	 ')) ; 
+INSERT INTO nacionalidad VALUES(	48	,TRIM('	marroquí	 ')) ; 
+INSERT INTO nacionalidad VALUES(	49	,TRIM('	mexicano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	50	,TRIM('	nicaragüense	 ')) ; 
+INSERT INTO nacionalidad VALUES(	51	,TRIM('	noruego	 ')) ; 
+INSERT INTO nacionalidad VALUES(	52	,TRIM('	neozelandés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	53	,TRIM('	panameño	 ')) ; 
+INSERT INTO nacionalidad VALUES(	54	,TRIM('	paraguayo	 ')) ; 
+INSERT INTO nacionalidad VALUES(	55	,TRIM('	peruano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	56	,TRIM('	polaco	 ')) ; 
+INSERT INTO nacionalidad VALUES(	57	,TRIM('	portugués	 ')) ; 
+INSERT INTO nacionalidad VALUES(	58	,TRIM('	puertorriqueño	 ')) ; 
+INSERT INTO nacionalidad VALUES(	59	,TRIM('	dominicano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	60	,TRIM('	rumano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	61	,TRIM('	ruso	 ')) ; 
+INSERT INTO nacionalidad VALUES(	62	,TRIM('	sueco	 ')) ; 
+INSERT INTO nacionalidad VALUES(	63	,TRIM('	suizo	 ')) ; 
+INSERT INTO nacionalidad VALUES(	64	,TRIM('	tailandés	 ')) ; 
+INSERT INTO nacionalidad VALUES(	65	,TRIM('	taiwanes	 ')) ; 
+INSERT INTO nacionalidad VALUES(	66	,TRIM('	turco	 ')) ; 
+INSERT INTO nacionalidad VALUES(	67	,TRIM('	ucraniano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	68	,TRIM('	uruguayo	 ')) ; 
+INSERT INTO nacionalidad VALUES(	69	,TRIM('	venezolano	 ')) ; 
+INSERT INTO nacionalidad VALUES(	70	,TRIM('	vietnamita	 ')) ; 
+
+INSERT INTO nivel_estudio VALUES (1, 'Educación básica Completa');
+INSERT INTO nivel_estudio VALUES (2, 'Educación básica Incompleta');
+INSERT INTO nivel_estudio VALUES (3, 'Educación media tecnico - profesional Completa');
+INSERT INTO nivel_estudio VALUES (4, 'Educación media tecnico - profesional Incompleta');
+INSERT INTO nivel_estudio VALUES (5, 'Educación media cientifico - humanisata Completa');
+INSERT INTO nivel_estudio VALUES (6, 'Educación media cientifico - humanisata Incompleta');
+INSERT INTO nivel_estudio VALUES (7, 'Educación superior nivel tecnico Completa');
+INSERT INTO nivel_estudio VALUES (8, 'Educación superior nivel tecnico Incompleta');
+INSERT INTO nivel_estudio VALUES (9, 'Educación superior nivel profesional Completa');
+INSERT INTO nivel_estudio VALUES (10, 'Educación superior nivel profesional Incompleta');
+INSERT INTO nivel_estudio VALUES (11, 'Maestría Completa');
+INSERT INTO nivel_estudio VALUES (12, 'Maestría Incompleta');
+INSERT INTO nivel_estudio VALUES (13, 'Doctorado Completo');
+INSERT INTO nivel_estudio VALUES (14, 'Maestría Incompleto');
+
+INSERT INTO tipo_documento VALUES(1, 'Rut');
+INSERT INTO tipo_documento VALUES(2, 'Pasaporte');
+INSERT INTO tipo_documento VALUES(3, 'Visa');
+
+INSERT INTO empresa (nombre_empresa, rut_empresa, imagen_empresa) VALUES ('ADMINISTRADORA DE BENEFICIOS PHARMA BENEFITS CHILE LTDA.','96838800-2','aaaa');
+INSERT INTO empresa (nombre_empresa, rut_empresa, imagen_empresa) VALUES ('CAJA DE COMPENSACION DE ASIGNACION FAMILIAR LOS HEROES','70016330-K','aaa');
+INSERT INTO empresa (nombre_empresa, rut_empresa, imagen_empresa) VALUES ('COMERCIALIZADORA S.A.','81675600-6','aaa');
+
+INSERT INTO control_empresa (usuario, clave, rut_empresa, id_empresa) VALUES ('96838800-2','96838800-2','96838800-2',1);
+INSERT INTO control_empresa (usuario, clave, rut_empresa, id_empresa) VALUES ('70016330-K','70016330-K','70016330-K',2);
+INSERT INTO control_empresa (usuario, clave, rut_empresa, id_empresa) VALUES ('81675600-6','81675600-6','81675600-6',3);
+
+INSERT INTO categoria_oferta VALUES (1, 'part time');
+INSERT INTO categoria_oferta VALUES (2, 'full time');
+INSERT INTO categoria_oferta VALUES (3, 'discapacitados');
+INSERT INTO categoria_oferta VALUES (4, 'practica profesional');
+
+INSERT INTO oferta_laboral (nombre_oferta, vacantes, id_empresa, glosa, id_comuna)
+VALUES ('Profesionales de obra', 20, 1, '4 - 21 años de experiencia', 7);
+INSERT INTO oferta_laboral (nombre_oferta, vacantes, id_empresa, glosa, id_comuna)
+VALUES ('Pogramador', 5, 2, '1 - 0 años de experiencia', 1);
+INSERT INTO oferta_laboral (nombre_oferta, vacantes, id_empresa, glosa, id_comuna)
+VALUES ('Vendedor técnico', 10, 3, '2 - 5 años de experiencia', 24);
+
+INSERT INTO detalle_categoria (id_categoria, id_oferta) VALUES (3,1);
+INSERT INTO detalle_categoria (id_categoria, id_oferta) VALUES (2,2);
+INSERT INTO detalle_categoria (id_categoria, id_oferta) VALUES (1,3);
+
+INSERT INTO pregunta (nombre_pregunta, id_oferta) VALUES ('lenguajes que domine', 2);
+INSERT INTO pregunta (nombre_pregunta, id_oferta) VALUES ('expectativas de renta', 1);
+INSERT INTO pregunta (nombre_pregunta, id_oferta) VALUES ('expectativas de renta', 3);
+INSERT INTO pregunta (nombre_pregunta, id_oferta) VALUES ('nivel de inglés', 3);
+
+INSERT INTO persona (   rut_persona, 
+                        id_tipo_d, 
+                        nombres, 
+                        apellidos, 
+                        fecha_nacimiento,
+                        id_nacionalidad,
+                        id_comuna,
+                        direccion,
+                        id_estadoc,
+                        id_sexo,
+                        id_genero,
+                        id_nivel_e,
+                        profesion,
+                        correo,
+                        telefono,
+                        celular,
+                        prestacion) 
+VALUES (    '19.782.810-2',
+            1,
+            'Byron Ricardo',
+            'Sepúlveda Sepúlveda',
+            '1997-09-11',
+            11,
+            39,
+            'santa luisa 1661',
+            1,
+            1,
+            1,
+            8,
+            'estudiante',
+            'by.sepulvedas@gmail.com',
+            null,
+            '948874894',
+            'byron');
+            
+INSERT INTO persona (   rut_persona, 
+                        id_tipo_d, 
+                        nombres, 
+                        apellidos, 
+                        fecha_nacimiento,
+                        id_nacionalidad,
+                        id_comuna,
+                        direccion,
+                        id_estadoc,
+                        id_sexo,
+                        id_genero,
+                        id_nivel_e,
+                        profesion,
+                        correo,
+                        telefono,
+                        celular,
+                        prestacion) 
+VALUES (    '20.181.487-1',
+            1,
+            'Mati',
+            'Larrondo',
+            '1999-03-25',
+            11,
+            39,
+            'su casa',
+            1,
+            1,
+            1,
+            8,
+            'estudiante',
+            'matimatimati@gmail.com',
+            null,
+            '99999999',
+            'mati');            
+
+INSERT INTO control_usuario (usuario, clave, rut_persona, id_persona) VALUES ('19.782.810-2','19.782.810-2','19.782.810-2',1);
+INSERT INTO control_usuario (usuario, clave, rut_persona, id_persona) VALUES ('20.181.487-1','20.181.487-1','20.181.487-1',2);
+
+INSERT INTO experiencia_laboral (id_persona, periodo, empresa, cargo, glosa) 
+VALUES (1, '2016 - 2017', 'Prinsa', 'operario',null); 
+INSERT INTO experiencia_laboral (id_persona, periodo, empresa, cargo, glosa) 
+VALUES (1, '2017 - 2017', 'Tata', 'digitalizador',null);
+INSERT INTO experiencia_laboral (id_persona, periodo, empresa, cargo, glosa) 
+VALUES (1, '2017 - 2017', 'Soporte tecnico', 'soporte',null);
+
+INSERT INTO likes (id_persona, id_oferta) VALUES (1,1);
+INSERT INTO likes (id_persona, id_oferta) VALUES (2,2);
+
+INSERT INTO postulacion (fecha, id_persona, id_oferta) VALUES (Now(), 1, 1);
+INSERT INTO postulacion (fecha, id_persona, id_oferta) VALUES (Now(), 2, 2);
+INSERT INTO postulacion (fecha, id_persona, id_oferta) VALUES (Now(), 1, 3);
+
+INSERT INTO respuestas (respuesta, id_postulacion, id_pregunta)
+VALUES ('300.000',1,2);
+INSERT INTO respuestas (respuesta, id_postulacion, id_pregunta)
+VALUES ('MEDIO',2,4);
+INSERT INTO respuestas (respuesta, id_postulacion, id_pregunta)
+VALUES ('500.000',3,3);
